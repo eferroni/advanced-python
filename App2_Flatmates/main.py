fa6956a555e455c9a4beb4e9c8bff4aaaa727184
eduardo-ferroni <eduard.ferroni@hp.com>
2022-09-02 17:53:18 -0300

    finish section 7

--- App2_Flatmates/main.py
+++ App2_Flatmates/main.py
@@ -1,7 +1,41 @@
 
-class Bill():
-    def __init__(self, amount):
+class Bill:
+    """
+    Objetc that contains data about a bill, such as
+    total amount and period of the bill
+    """
+
+    def __init__(self, amount, period):
         self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Object that contains data about a flatmate person
+    who lives in the house and pays share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
+
+
+class PdfReport:
+    """
+    Create a pdf file that contains data abput
+    the flatmates such as their names, their amount
+    and the period of the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
 
 
 
