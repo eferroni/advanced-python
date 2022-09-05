6ca7580c0935baf3f79a81eb5714de488451a057
eduardo-ferroni <eduard.ferroni@hp.com>
2022-09-05 10:37:33 -0300

    finish section 8

--- App2_Flatmates/main.py
+++ App2_Flatmates/main.py
@@ -1,5 +1,8 @@
-import month
+import webbrowser
 
+import month
+from fpdf import FPDF
+import calendar
 
 class Bill:
     """
@@ -65,7 +68,62 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, bill: Bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image("house.png", w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmate Bill', border=1, align='C', ln=1)
+
+        # Insert Period label and value
+        pdf.set_font('Times', size=14, style='B')
+        pdf.cell(
+            w=300,
+            h=40,
+            txt=f'Period: {calendar.month_name[bill.month]}/{bill.year}',
+            border=0,
+        )
+        pdf.cell(
+            w=250,
+            h=40,
+            txt=f'Total Amount: $ {bill.amount}',
+            border=0,
+            ln=1
+        )
+
+        # Labels
+        pdf.set_font(family="Times", size=12, style='B')
+        pdf.cell(w=150, h=25, txt="Name")
+        pdf.cell(w=150, h=25, txt="Days in House")
+        pdf.cell(w=150, h=25, txt="Pay", ln=1)
+
+        # Insert Flatmates
+        pdf.set_font(family="Times", size=12)
+        for flatmate in bill.flatmates:
+            pdf.cell(
+                w=150,
+                h=25,
+                txt=flatmate.name,
+                border=0,
+            )
+            pdf.cell(
+                w=150,
+                h=25,
+                txt=str(flatmate.days_in_house[bill.year][bill.month]["days"]),
+                border=0,
+            )
+            pdf.cell(
+                w=150,
+                h=25,
+                txt=f'$ {flatmate.days_in_house[bill.year][bill.month]["amount"]}',
+                border=0,
+                ln=1
+            )
+
+        pdf.output(self.filename)
 
 
 john = Flatmate(name="John")
@@ -76,4 +134,6 @@ mary.add_days_in_house(2022, month.MARCH, 25)
 bill = Bill(120, 2022, month.MARCH, john, mary)
 bill.calculate_flatmate_bill()
 
+pdf = PdfReport('bill.pdf')
+pdf.generate(bill)
 
