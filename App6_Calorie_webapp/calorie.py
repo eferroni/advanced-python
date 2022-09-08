fd76f719513833b9befade1c8c392a2272081be8
eduardo-ferroni <eduard.ferroni@hp.com>
2022-09-08 14:16:08 -0300

    finish section 27

---
+++ App6_Calorie_webapp/calorie.py
@@ -0,0 +1,9 @@
+class Calorie:
+    def __int__(self, weight, height, age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        pass
---
+++ App6_Calorie_webapp/temperature.py
@@ -0,0 +1,7 @@
+class Temperature:
+    def __init__(self, country, city):
+        self.country = country
+        self.city = city
+
+    def get(self):
+        pass
