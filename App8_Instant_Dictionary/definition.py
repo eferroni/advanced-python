80db4e655e8491b3cda2d135ca81d84f2aeffe9d
eduardo-ferroni <eduard.ferroni@hp.com>
2022-09-09 17:07:01 -0300

    finish section 41

---
+++ App8_Instant_Dictionary/definition.py
@@ -0,0 +1,15 @@
+import pandas
+
+
+class Definition:
+
+    def __init__(self, term: str) -> None:
+        self.term = term.lower()
+
+    def get(self):
+        df = pandas.read_csv('data.csv')
+        return tuple(df.loc[df['word'] == self.term]['definition'])
+
+
+d = Definition(term='sun')
+print(d.get())
